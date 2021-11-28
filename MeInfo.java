276384048ddd4dd09a949babfd5f8169fd427bdc
Muhammad Shahbaz <M023Shah@gmail.com>
2021-11-27 21:20:22 -0800

    initial commit

---
+++ MeInfo.java
@@ -0,0 +1,10 @@
+public class MeInfo {
+    public static void main(String[] args) {
+        String x = "Muhammad Shahbaz";
+        String age = "27";
+        String town = "Sacramento, CA";
+        System.out.println("My name is " + x );
+        System.out.println("I am a " + age + "years old" );
+        System.out.println("My hometown is " + town );
+    }
+}
